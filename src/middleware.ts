7c646112461b3b98109d57039bfc328a1dcc6432
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2022-09-06 11:49:03 +0530

    create edge function to redirect

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+  if (
+    req.nextUrl.pathname.startsWith("/api/") ||
+    req.nextUrl.pathname === "/"
+  ) {
+    return;
+  }
+
+  const slug = req.nextUrl.pathname.split("/").pop();
+  const fetchSlug = await fetch(`${req.nextUrl.origin}/api/get-link/${slug}`);
+
+  if (fetchSlug.status === 404) {
+    return NextResponse.redirect(req.nextUrl.origin);
+  }
+
+  const data = await fetchSlug.json();
+
+  if (data?.url) {
+    return NextResponse.redirect(data.url);
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/:slug",
+};
