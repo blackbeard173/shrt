bae329b007ffa2de7a0b7915be6acfe6185e449f
Blackbeard <67589385+blackbeard173@users.noreply.github.com>
2022-09-06 13:31:01 +0530

    update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,14 @@
 import { NextFetchEvent, NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, event: NextFetchEvent) {
+  if (
+    req.nextUrl.pathname.startsWith("/api/") ||
+    req.nextUrl.pathname === "/"
+  ) {
+    return;
+  }
   const slug = req.nextUrl.pathname.split("/").pop();
+
   const fetchSlug = await fetch(`${req.nextUrl.origin}/api/get-link/${slug}`);
 
   if (fetchSlug.status === 404) {
