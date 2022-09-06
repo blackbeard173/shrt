adf35256309dc329833d18dedb2685bd2a65af11
BlackBeard <67589385+blackbeard173@users.noreply.github.com>
2022-09-06 13:38:06 +0530

    Update [slug].ts

--- src/pages/api/get-link/[slug].ts
+++ src/pages/api/get-link/[slug].ts
@@ -29,5 +29,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=1000000000, stale-while-revalidate");
 
-  return res.json(data);
+  res.json(data);
+  
+  return;
 };
